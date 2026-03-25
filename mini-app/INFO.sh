812a47369de4a49e08ece272f3c476d37577892c
caoyang2002 <cy12968@163.com>
2026-03-25 08:29:53 +0800

    fix [info]: fix INFO bug

--- mini-app/INFO.sh
+++ mini-app/INFO.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# INFO.sh
+# Copyright (c) 2000-2020 Synology Inc. All rights reserved.
+
 source /pkgscripts/include/pkg_util.sh
-package="MiniDemo"
-version="1.0.0000"
-os_min_ver="7.0-40000"
-displayname="Mini Demo Package"
-description="this is an minimal demo package"
-arch="$(pkg_get_unified_platform)"
+
+# 包信息
+package="mini-app"
+version="1.0.0"
+
+# 使用 pkg_get_platform_family 获取架构（返回 x86_64）
+arch="$(pkg_get_platform_family)"
+
 maintainer="Synocatjs"
+distributor="Synocatjs"
+description="A minimal Synology DSM package demo"
+displayname="Mini App"
+os_min_ver="7.0-40000"
+
+# 可选字段
+silent_install="yes"
+silent_upgrade="yes"
+silent_uninstall="yes"
+
+# 生成 INFO 文件
 pkg_dump_info
